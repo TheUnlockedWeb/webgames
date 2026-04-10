69b5412b70e2ed7d0499878b3666e05ff6120856
EthanUnlocked <ethan.ioannou30@student.ccgs.nsw.edu.au>
2026-04-10 22:29:50 +1000

    Create device.action.ts

---
+++ pokegobreeze/src/store/actions/device.action.ts
@@ -0,0 +1,27 @@
+import { isMobile, browserName, osName } from 'react-device-detect';
+import { Action } from 'redux';
+import { DeviceModel } from '../reducers/device.reducer';
+
+export enum DeviceActionTypes {
+  setDevice = '[Device] SetDevice',
+}
+
+export class SetDevice implements Action {
+  readonly type = DeviceActionTypes.setDevice;
+
+  constructor(public payload: DeviceModel) {}
+
+  static create() {
+    const { type, payload } = new SetDevice({
+      isMobile,
+      browserName,
+      osName,
+    });
+    return {
+      type,
+      payload,
+    };
+  }
+}
+
+export type DeviceActionsUnion = SetDevice;
