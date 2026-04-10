bd21b1698d5fc21ea398639c93ee7abde6273311
EthanUnlocked <ethan.ioannou30@student.ccgs.nsw.edu.au>
2026-04-10 22:40:02 +1000

    Create data-table.model.ts

---
+++ pokegobreeze/src/utils/models/overrides/data-table.model.ts
@@ -0,0 +1,14 @@
+import { TableColumn } from 'react-data-table-component';
+
+type Primitive = string | number | boolean | JSX.Element | JSX.Element[];
+type Selector<T> = (row: T, rowIndex?: number) => Primitive;
+type TableColumnOverride<T> = Omit<TableColumn<T>, 'selector'> & { selector?: Selector<T> };
+
+export interface TableColumnModify<T> extends TableColumnOverride<T> {
+  selector?: Selector<T>;
+}
+
+export enum SortOrderType {
+  ASC = 'asc',
+  DESC = 'desc',
+}
