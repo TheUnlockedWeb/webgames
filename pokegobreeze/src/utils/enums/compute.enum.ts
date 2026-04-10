07567f1f29416243dc68eedce2b6fc27688690e9
EthanUnlocked <ethan.ioannou30@student.ccgs.nsw.edu.au>
2026-04-10 22:44:13 +1000

    Create compute.enum.ts

---
+++ pokegobreeze/src/utils/enums/compute.enum.ts
@@ -0,0 +1,24 @@
+export enum BattleLeagueCPType {
+  Master,
+  Little = 500,
+  Great = 1500,
+  Ultra = 2500,
+  InsMaster = 10000,
+}
+
+export enum BattleLeagueIconType {
+  Little = 'GBL_littlecup',
+  Great = 'great_league',
+  Ultra = 'ultra_league',
+  Master = 'master_league',
+}
+
+export enum BattleLeagueTag {
+  Seeker = 'seeker',
+  SafariZone = 'safari_zone',
+}
+
+export enum FormType {
+  Default,
+  Shiny,
+}
