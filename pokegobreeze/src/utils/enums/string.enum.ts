df72fb1c074870e9d3f75a0f6841ec1defc7a8f4
EthanUnlocked <ethan.ioannou30@student.ccgs.nsw.edu.au>
2026-04-10 22:44:46 +1000

    Create string.enum.ts

---
+++ pokegobreeze/src/utils/enums/string.enum.ts
@@ -0,0 +1,16 @@
+export enum EqualMode {
+  CaseSensitive = 1,
+  IgnoreCaseSensitive,
+}
+
+export enum IncludeMode {
+  Include = 1,
+  IncludeIgnoreCaseSensitive,
+  IncludeBetween,
+  IncludeBetweenIgnoreCaseSensitive,
+}
+
+export enum PaddingMode {
+  Start = 1,
+  End,
+}
