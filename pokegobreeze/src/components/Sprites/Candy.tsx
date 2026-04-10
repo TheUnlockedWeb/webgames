2a4c0843f6e0e0fe8572441d5af281db086102e2
EthanUnlocked <ethan.ioannou30@student.ccgs.nsw.edu.au>
2026-04-10 15:28:32 +1000

    Create Candy.tsx

---
+++ pokegobreeze/src/components/Sprites/Candy.tsx
@@ -0,0 +1,55 @@
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import { computeCandyBgColor, computeCandyColor } from '../../../utils/compute';
+
+import bgCandy from '../../../assets/bg_Candy.png';
+
+import { ICandyComponent } from '../../models/component.model';
+import { toNumber } from '../../../utils/extension';
+
+const DEFAULT_SIZE = 20;
+
+interface Element {
+  $candyColor?: string;
+  $candyBgColor?: string;
+  $size?: number;
+}
+
+const Background = styled.div<Element>`
+  display: inline-block;
+  background-color: ${(props) => props.$candyBgColor};
+  border-radius: 50%;
+  width: fit-content;
+  height: fit-content;
+`;
+
+const Fill = styled.div<Element>`
+  background: ${(props) => props.$candyColor};
+  width: ${(props) => toNumber(props.$size, DEFAULT_SIZE)}px;
+  height: ${(props) => toNumber(props.$size, DEFAULT_SIZE)}px;
+  mask: url(${bgCandy}) center/contain;
+  -webkit-mask: url(${bgCandy}) center/contain;
+  -moz-mask: url(${bgCandy}) center/contain;
+  -o-mask: url(${bgCandy}) center/contain;
+  -ms-mask: url(${bgCandy}) center/contain;
+`;
+
+const Candy = (props: ICandyComponent) => {
+  const [color, setColor] = useState<string>();
+  const [bgColor, setBgColor] = useState<string>();
+
+  useEffect(() => {
+    const candyColor = computeCandyColor(props.id);
+    const candyBgColor = computeCandyBgColor(props.id);
+    setColor(candyColor);
+    setBgColor(candyBgColor);
+  }, [props.id]);
+
+  return (
+    <Background style={props.style} className={props.className} $candyBgColor={bgColor}>
+      <Fill $candyColor={color} $size={props.size} />
+    </Background>
+  );
+};
+
+export default Candy;
