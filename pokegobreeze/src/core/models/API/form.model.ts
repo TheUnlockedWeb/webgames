1821c07dbf6b231dbaf68b02460ac6a6a2407ec0
EthanUnlocked <ethan.ioannou30@student.ccgs.nsw.edu.au>
2026-04-10 19:38:20 +1000

    Create form.model.ts

---
+++ pokegobreeze/src/core/models/API/form.model.ts
@@ -0,0 +1,234 @@
+import { PokemonType } from '../../../enums/type.enum';
+import { DynamicObj, getValueOrDefault, isEqual, isNotEmpty } from '../../../utils/extension';
+import { formNormal } from '../../../utils/helpers/options-context.helpers';
+import { convertPokemonAPIDataFormName, getPokemonType } from '../../../utils/utils';
+import { IStatsPokemon } from '../stats.model';
+import { IPokemonDetail, SpriteInfo } from './info.model';
+import { Path } from './species.model';
+
+export interface IPokemonSprite {
+  backDefault: string;
+  backFemale: string;
+  backShiny: string;
+  backShinyFemale: string;
+  frontDefault: string;
+  frontFemale: string;
+  frontShiny: string;
+  frontShinyFemale: string;
+}
+
+export class PokemonSprite implements IPokemonSprite {
+  backDefault = '';
+  backFemale = '';
+  backShiny = '';
+  backShinyFemale = '';
+  frontDefault = '';
+  frontFemale = '';
+  frontShiny = '';
+  frontShinyFemale = '';
+
+  static setDetails(info: SpriteInfo | undefined) {
+    const obj = new PokemonSprite();
+    if (info) {
+      obj.backDefault = info.back_default;
+      obj.backFemale = getValueOrDefault(String, info.back_female);
+      obj.backShiny = getValueOrDefault(String, info.back_shiny);
+      obj.backShinyFemale = getValueOrDefault(String, info.back_shiny_female);
+      obj.frontDefault = info.front_default;
+      obj.frontFemale = getValueOrDefault(String, info.front_female);
+      obj.frontShiny = getValueOrDefault(String, info.front_shiny);
+      obj.frontShinyFemale = getValueOrDefault(String, info.front_shiny_female);
+    }
+    return obj;
+  }
+}
+
+export interface PokemonForm {
+  form_name: string;
+  form_names: string[];
+  form_order: number;
+  id: number;
+  is_battle_only: boolean;
+  is_default: boolean;
+  is_mega: boolean;
+  name: string;
+  names: string[];
+  order: number;
+  pokemon: Path;
+  sprites: SpriteInfo | undefined;
+  types: SlotType[];
+  version_group: Path;
+}
+
+export interface IPokemonFormDetail {
+  formName: string;
+  id: number;
+  isDefault: boolean;
+  pokemonType: PokemonType;
+  name: string;
+  pokemon: Path;
+  sprites?: IPokemonSprite;
+  types: string[];
+  version: string | undefined;
+}
+
+export class PokemonFormDetail implements IPokemonFormDetail {
+  formName = '';
+  id = 0;
+  isDefault = false;
+  pokemonType = PokemonType.None;
+  name = '';
+  pokemon!: Path;
+  sprites: IPokemonSprite | undefined;
+  types: string[] = [];
+  version = '';
+
+  static setDetails(info: PokemonForm) {
+    const obj = new PokemonFormDetail();
+    obj.formName = info.form_name;
+    obj.id = info.id;
+    obj.isDefault = info.is_default;
+    obj.pokemonType = getPokemonType(obj.formName, info.is_mega);
+    obj.name = info.name;
+    obj.pokemon = info.pokemon;
+    obj.sprites = PokemonSprite.setDetails(info.sprites);
+    obj.types = info.types.map((t) => t.type.name);
+    obj.version = info.version_group.name;
+    return obj;
+  }
+}
+
+interface SlotType {
+  slot: number;
+  type: Path;
+}
+
+export interface IForm extends Partial<IPokemonFormDetail> {
+  formName: string | undefined;
+  id: number | undefined;
+  isDefault: boolean;
+  pokemonType?: PokemonType;
+  name: string;
+  version: string | undefined;
+  types: string[] | undefined;
+}
+
+export interface IPokemonFormModify {
+  defaultId: number;
+  defaultName: string;
+  name: string;
+  form: IForm;
+  sprites?: IPokemonSprite;
+}
+
+export class PokemonFormModify implements IPokemonFormModify {
+  defaultId = 0;
+  defaultName = '';
+  name = '';
+  form = new Form();
+
+  static setForm(defaultId: number, defaultName: string, name: string | undefined, form: Form) {
+    const obj = new PokemonFormModify();
+    obj.defaultId = defaultId;
+    obj.defaultName = defaultName;
+    obj.name = getValueOrDefault(String, name);
+    obj.form = form;
+    return obj;
+  }
+
+  static create(value: IPokemonFormModify) {
+    const obj = new PokemonFormModify();
+    Object.assign(obj, value);
+    return obj;
+  }
+}
+
+export interface PokemonDataForm {
+  stats: IStatsPokemon;
+  num: number;
+  types: string[] | undefined;
+}
+
+export class PokemonFormModifyModel implements IPokemonFormModify {
+  defaultId: number;
+  defaultName: string;
+  name: string;
+  form: IForm;
+  sprites = new PokemonSprite();
+
+  constructor(
+    id: number,
+    defaultName: string,
+    name: string | undefined,
+    formName: string | undefined,
+    fullFormName: string | undefined,
+    version: string | undefined,
+    types: string[],
+    sprites: IPokemonSprite | undefined,
+    formId: number,
+    pokemonType = PokemonType.Normal,
+    isDefault = true
+  ) {
+    this.defaultId = id;
+    this.defaultName = defaultName;
+    this.name = getValueOrDefault(String, name);
+    this.form = Form.create({
+      formName,
+      id: formId,
+      isDefault,
+      pokemonType,
+      name: getValueOrDefault(String, fullFormName),
+      version,
+      types,
+      sprites,
+    });
+  }
+}
+
+export class Form implements IForm {
+  formName: string | undefined;
+  id: number | undefined;
+  isDefault = false;
+  pokemonType = PokemonType.Normal;
+  name = '';
+  version: string | undefined;
+  types: string[] | undefined = [];
+
+  static create(value: IForm, isMega = false, isShadow = true) {
+    const obj = new Form();
+    obj.pokemonType = getPokemonType(value.formName, isMega, isShadow);
+    Object.assign(obj, value);
+    return obj;
+  }
+
+  static setValue(value: IPokemonFormDetail, name: string) {
+    const obj = new Form();
+    const formName = isEqual(value.pokemonType, PokemonType.GMax)
+      ? value.name.replace(`${name}-`, '')
+      : convertPokemonAPIDataFormName(value.formName, value.name);
+    if (formName) {
+      value.formName = formName;
+    }
+    Object.assign(obj, value);
+    return obj;
+  }
+}
+
+export interface IFormSoundCry {
+  form: string;
+  cries?: DynamicObj<string>;
+}
+
+export class FormSoundCry implements IFormSoundCry {
+  form: string;
+  cries?: DynamicObj<string>;
+
+  constructor(pokemon: IPokemonDetail) {
+    const fullName = isNotEmpty(pokemon.forms) ? pokemon.forms[0].name : '';
+    const speciesName = pokemon.speciesName;
+    this.form = isEqual(fullName, speciesName)
+      ? formNormal()
+      : fullName.replace(`${speciesName}-`, '').replaceAll('-', '_').toUpperCase();
+    this.cries = pokemon.cries;
+  }
+}
