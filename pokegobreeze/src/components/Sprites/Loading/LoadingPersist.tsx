fa1bf6f12c8a6f17a1ac910567dfb6a9fe8d51e4
EthanUnlocked <ethan.ioannou30@student.ccgs.nsw.edu.au>
2026-04-10 15:35:42 +1000

    Create LoadingPersist.tsx

---
+++ pokegobreeze/src/components/Sprites/Loading/LoadingPersist.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ResponsiveAppBar from '../../Commons/Navbars/ResponsiveAppBar';
+
+const LoadingPersist = () => {
+  return <ResponsiveAppBar />;
+};
+
+export default LoadingPersist;
