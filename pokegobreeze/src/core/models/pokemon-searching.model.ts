99eb76147b974d5558aa0d5428e5fb5b336ff7bd
EthanUnlocked <ethan.ioannou30@student.ccgs.nsw.edu.au>
2026-04-10 19:27:33 +1000

    Create pokemon-searching.model.ts

---
+++ pokegobreeze/src/core/models/pokemon-searching.model.ts
@@ -0,0 +1,24 @@
+import APIService from '../../services/api.service';
+import { splitAndCapitalize } from '../../utils/utils';
+import { IPokemonData } from './pokemon.model';
+
+export interface IPokemonSearching {
+  id: number;
+  name: string;
+  sprites: string;
+}
+
+export class PokemonSearching implements IPokemonSearching {
+  id: number;
+  name: string;
+  sprites: string;
+
+  constructor(item: IPokemonData) {
+    this.id = item.num;
+    this.name = splitAndCapitalize(item.pokemonId?.toString().replace(/-M$/, 'MALE').replace(/-F$/, 'FEMALE'), '_', ' ')
+      .replace('Mr ', 'Mr. ')
+      .replace(/^Ho Oh$/, 'Ho-Oh')
+      .replace(/ O$/, '-O');
+    this.sprites = APIService.getPokeSprite(item.num);
+  }
+}
