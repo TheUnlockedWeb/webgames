d596c3dc23f09f91f63a42fa14711ee6508fb279
EthanUnlocked <ethan.ioannou30@student.ccgs.nsw.edu.au>
2026-04-10 22:40:20 +1000

    Create dom.model.ts

---
+++ pokegobreeze/src/utils/models/overrides/dom.model.ts
@@ -0,0 +1,46 @@
+export interface ScrollModifyEvent extends Event {
+  target: TargetModify | null;
+}
+
+type TargetModify = Omit<EventTargetModify, 'documentElement'> & {
+  documentElement?: HTMLElement;
+};
+type EventTargetModify = Omit<EventTarget, 'scrollingElement'> & {
+  scrollingElement?: HTMLElement;
+};
+
+interface TouchList {
+  [index: number]: Touch;
+  length: number;
+  item(index: number): Touch;
+  identifiedTouch(identifier: number): Touch;
+}
+
+interface Touch {
+  identifier: number;
+  target: EventTarget;
+  clientX: number;
+  clientY: number;
+  screenX: number;
+  screenY: number;
+  pageX: number;
+  pageY: number;
+  force: number;
+  radiusX: number;
+  radiusY: number;
+  rotationAngle: number;
+}
+
+export interface TimelineEventBase<T> {
+  changedTouches?: TouchList;
+  currentTarget: EventTarget & T;
+  nativeEvent: MouseEvent | TouchEvent;
+  clientX?: number;
+}
+
+export type TimelineEvent<T> = (React.MouseEvent<T> | React.TouchEvent<T>) & TimelineEventBase<T>;
+
+export interface TimelineElement<T> {
+  (e: TimelineEvent<T>): void;
+  bind<T>(this: T, thisArg: ThisParameterType<T>): OmitThisParameter<T>;
+}
