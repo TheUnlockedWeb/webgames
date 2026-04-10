03521102a94fe58bfda6db8a574be397941b153a
EthanUnlocked <ethan.ioannou30@student.ccgs.nsw.edu.au>
2026-04-10 17:19:16 +1000

    Create information.enum.ts

---
+++ pokegobreeze/src/core/enums/information.enum.ts
@@ -0,0 +1,21 @@
+export enum TicketRewardType {
+  None,
+  Item,
+  Pokemon,
+  PokeCoin,
+  Exp,
+  Stardust,
+  Avatar,
+  Candy,
+}
+
+export enum ItemTicketRewardType {
+  Item = 'ITEM',
+  Pokemon = 'POKEMON_ENCOUNTER',
+  PokeCoin = 'POKECOIN',
+  Exp = 'EXPERIENCE',
+  Stardust = 'STARDUST',
+  Avatar = 'AVATAR_CLOTHING',
+  BattlePass = 'BATTLE_PASS_TICKET',
+  Candy = 'CANDY',
+}
