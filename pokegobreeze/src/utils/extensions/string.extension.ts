79406f9cbe0b3d5727dc2f3e8005f0a72455333d
EthanUnlocked <ethan.ioannou30@student.ccgs.nsw.edu.au>
2026-04-10 22:43:45 +1000

    Create string.extension.ts

---
+++ pokegobreeze/src/utils/extensions/string.extension.ts
@@ -0,0 +1,36 @@
+declare global {
+  interface String {
+    isEmpty(): boolean;
+    isNull(): boolean;
+    isUndefined(): boolean;
+    isEqual(value: string): boolean;
+    isEqualWithIgnoreCase(value: string): boolean;
+    includesWithIgnoreCase(searchElement: string): boolean;
+  }
+}
+
+String.prototype.isEmpty = function () {
+  return typeof this === 'string' && this === '';
+};
+
+String.prototype.isNull = function () {
+  return typeof this !== 'undefined' && this === null;
+};
+
+String.prototype.isUndefined = function () {
+  return typeof this === 'undefined' && this === undefined;
+};
+
+String.prototype.isEqual = function (value: string) {
+  return this === value;
+};
+
+String.prototype.isEqualWithIgnoreCase = function (value: string) {
+  return this.toUpperCase() === value.toUpperCase();
+};
+
+String.prototype.includesWithIgnoreCase = function (searchElement: string) {
+  return this.toUpperCase().includes(searchElement.toUpperCase());
+};
+
+export {};
