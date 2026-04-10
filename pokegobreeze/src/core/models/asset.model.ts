7bc2ec4bc9fab6c45a874b1ee5d38f588050ced6
EthanUnlocked <ethan.ioannou30@student.ccgs.nsw.edu.au>
2026-04-10 17:21:06 +1000

    Create asset.model.ts

---
+++ pokegobreeze/src/core/models/asset.model.ts
@@ -0,0 +1,69 @@
+import { PokemonType } from '../../enums/type.enum';
+import { getPokemonType } from '../../utils/utils';
+import { GenderType } from '../enums/asset.enum';
+
+export interface IImage {
+  gender?: GenderType;
+  pokemonId?: number;
+  form?: string;
+  default: string;
+  shiny: string | undefined;
+  pokemonType?: PokemonType;
+}
+
+export class ImageModel implements IImage {
+  gender?: GenderType;
+  pokemonId?: number;
+  form?: string;
+  default = '';
+  shiny = '';
+  pokemonType = PokemonType.Normal;
+
+  constructor({ ...props }: IImage) {
+    if (!props.pokemonType) {
+      props.pokemonType = getPokemonType(props.form);
+    }
+    Object.assign(this, props);
+  }
+}
+
+export interface ICryPath {
+  form: string;
+  path: string;
+  pokemonType?: PokemonType;
+}
+
+export class CryPath implements ICryPath {
+  form = '';
+  path = '';
+  pokemonType = PokemonType.Normal;
+
+  constructor({ ...props }: ICryPath) {
+    if (!props.pokemonType) {
+      props.pokemonType = getPokemonType(props.form);
+    }
+    Object.assign(this, props);
+  }
+}
+
+interface ISound {
+  cry: ICryPath[];
+}
+
+class Sound implements ISound {
+  cry: ICryPath[] = [];
+}
+
+export interface IAsset {
+  id?: number;
+  name: string;
+  image: IImage[];
+  sound: ISound;
+}
+
+export class Asset implements IAsset {
+  id = 0;
+  name = '';
+  image: IImage[] = [];
+  sound = new Sound();
+}
