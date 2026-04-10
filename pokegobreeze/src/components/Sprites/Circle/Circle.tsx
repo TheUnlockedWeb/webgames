ea1788be91c75135af63577d9673ab9b68fe1bfb
EthanUnlocked <ethan.ioannou30@student.ccgs.nsw.edu.au>
2026-04-10 15:30:23 +1000

    Create Circle.tsx

---
+++ pokegobreeze/src/components/Sprites/Circle/Circle.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import styled from 'styled-components';
+import { ICircleComponent } from '../../models/component.model';
+
+interface Element {
+  $line: number;
+  $size: number;
+  $color: string;
+}
+
+const Fill = styled.div<Element>`
+  border: ${(props) => props.$line}px solid ${(props) => props.$color};
+  border-radius: 50%;
+  width: ${(props) => props.$size}px;
+  height: ${(props) => props.$size}px;
+  transition: 0.1s;
+`;
+
+const Circle = (props: ICircleComponent) => {
+  return <Fill $line={props.line} $size={props.size} $color={props.color} />;
+};
+
+export default Circle;
