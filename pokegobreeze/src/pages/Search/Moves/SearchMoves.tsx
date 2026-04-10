753372b6dd949e60e1506fce61173bbf73fd20d1
EthanUnlocked <ethan.ioannou30@student.ccgs.nsw.edu.au>
2026-04-10 21:53:32 +1000

    Create SearchMoves.tsx

---
+++ pokegobreeze/src/pages/Search/Moves/SearchMoves.tsx
@@ -0,0 +1,264 @@
+import React, { useEffect, useState } from 'react';
+import { capitalize, createDataRows, getDataWithKey, getKeyWithData, splitAndCapitalize } from '../../../utils/utils';
+
+import './SearchMoves.scss';
+import { ColumnType, TypeMove } from '../../../enums/type.enum';
+import { ICombat } from '../../../core/models/combat.model';
+import { useTitle } from '../../../utils/hooks/useTitle';
+import { TableColumnModify } from '../../../utils/models/overrides/data-table.model';
+import {
+  combineClasses,
+  getValueOrDefault,
+  isEqual,
+  isInclude,
+  toFloat,
+  toFloatWithPadding,
+  toNumber,
+} from '../../../utils/extension';
+import { SelectType } from './enums/select-type.enum';
+import { EqualMode, IncludeMode } from '../../../utils/enums/string.enum';
+import { Params } from '../../../utils/constants';
+import { LinkToTop } from '../../../components/Link/LinkToTop';
+import { debounce } from 'lodash';
+import CircularProgressTable from '../../../components/Sprites/CircularProgress/CircularProgress';
+import CustomDataTable from '../../../components/Commons/Tables/CustomDataTable/CustomDataTable';
+import { PokemonTypeBadge } from '../../../core/enums/pokemon-type.enum';
+import { getTypes } from '../../../utils/helpers/options-context.helpers';
+import useCombats from '../../../composables/useCombats';
+import SelectMui from '../../../components/Commons/Selects/SelectMui';
+import InputMui from '../../../components/Commons/Inputs/InputMui';
+
+const nameSort = (rowA: ICombat, rowB: ICombat) => {
+  const a = rowA.name.toLowerCase();
+  const b = rowB.name.toLowerCase();
+  return a === b ? 0 : a > b ? 1 : -1;
+};
+
+const moveSort = (rowA: ICombat, rowB: ICombat) => {
+  const a = getValueOrDefault(String, rowA.type?.toLowerCase());
+  const b = getValueOrDefault(String, rowB.type?.toLowerCase());
+  return a === b ? 0 : a > b ? 1 : -1;
+};
+
+const numSortDps = (rowA: ICombat, rowB: ICombat) => {
+  const a = toFloat(rowA.pvePower / (rowA.durationMs / 1000));
+  const b = toFloat(rowB.pvePower / (rowB.durationMs / 1000));
+  return a - b;
+};
+
+const columns = createDataRows<TableColumnModify<ICombat>>(
+  {
+    id: ColumnType.Id,
+    name: 'id',
+    selector: (row) => row.track,
+    sortable: true,
+  },
+  {
+    id: ColumnType.Type,
+    name: 'Type',
+    selector: (row) => (
+      <div className={combineClasses('type-icon-small', row.type?.toLowerCase())}>{capitalize(row.type)}</div>
+    ),
+    sortable: true,
+    sortFunction: moveSort,
+  },
+  {
+    id: ColumnType.Name,
+    name: 'Name',
+    selector: (row) => (
+      <LinkToTop
+        to={`/move/${row.track}${row.isMultipleWithType ? `?${Params.MoveType}=${row.type?.toLowerCase()}` : ''}`}
+      >
+        {splitAndCapitalize(row.name, '_', ' ')}
+      </LinkToTop>
+    ),
+    sortable: true,
+    sortFunction: nameSort,
+    width: '180px',
+  },
+  {
+    id: ColumnType.Power,
+    name: 'Power (PVE/PVP)',
+    selector: (row) => `${row.pvePower}/${row.pvpPower}`,
+    sortable: true,
+    width: '150px',
+  },
+  {
+    id: ColumnType.DPS,
+    name: 'DPS',
+    selector: (row) => toFloatWithPadding(row.pvePower / (row.durationMs / 1000), 2),
+    sortFunction: numSortDps,
+    sortable: true,
+  }
+);
+
+interface IFilter {
+  fMoveType: SelectType;
+  fMoveName: string;
+  cMoveType: SelectType;
+  cMoveName: string;
+}
+
+class Filter implements IFilter {
+  fMoveType = SelectType.All;
+  fMoveName = '';
+  cMoveType = SelectType.All;
+  cMoveName = '';
+
+  static create(value: IFilter) {
+    const obj = new Filter();
+    Object.assign(obj, value);
+    return obj;
+  }
+}
+
+const Search = () => {
+  useTitle({
+    title: 'Moves - Search',
+    description:
+      'Search and filter Pokémon GO moves by type, power, energy, and more. Find the best moves for your Pokémon in battles and raids.',
+    keywords: ['Pokémon moves', 'move search', 'best moves', 'PVP moves', 'raid moves', 'Pokémon GO attacks'],
+  });
+  const { getCombatsByTypeMove } = useCombats();
+
+  const [filters, setFilters] = useState(new Filter());
+
+  const { fMoveType, fMoveName, cMoveType, cMoveName } = filters;
+
+  const [combatFMoves, setCombatFMoves] = useState<ICombat[]>([]);
+  const [resultFMove, setResultFMove] = useState<ICombat[]>([]);
+  const [fMoveIsLoad, setFMoveIsLoad] = useState(false);
+  const [combatCMoves, setCombatCMoves] = useState<ICombat[]>([]);
+  const [resultCMove, setResultCMove] = useState<ICombat[]>([]);
+  const [cMoveIsLoad, setCMoveIsLoad] = useState(false);
+
+  useEffect(() => {
+    setCombatFMoves(getCombatsByTypeMove(TypeMove.Fast));
+    setCombatCMoves(getCombatsByTypeMove(TypeMove.Charge));
+  }, [getCombatsByTypeMove]);
+
+  useEffect(() => {
+    const debounced = debounce(() => {
+      setResultFMove(searchMove(combatFMoves, fMoveType, fMoveName));
+      setFMoveIsLoad(true);
+    });
+    debounced();
+    return () => {
+      debounced.cancel();
+    };
+  }, [combatFMoves, fMoveType, fMoveName]);
+
+  useEffect(() => {
+    const debounced = debounce(() => {
+      setResultCMove(searchMove(combatCMoves, cMoveType, cMoveName));
+      setCMoveIsLoad(true);
+    });
+    debounced();
+    return () => {
+      debounced.cancel();
+    };
+  }, [combatCMoves, cMoveType, cMoveName]);
+
+  const searchMove = (combat: ICombat[], type: SelectType | PokemonTypeBadge, name: string) =>
+    combat.filter(
+      (move) =>
+        (isInclude(splitAndCapitalize(move.name, '_', ' '), name, IncludeMode.IncludeIgnoreCaseSensitive) ||
+          isInclude(move.track, name)) &&
+        (type === SelectType.All ||
+          isEqual(getKeyWithData(PokemonTypeBadge, type), move.type, EqualMode.IgnoreCaseSensitive))
+    );
+
+  const setMoveByType = (category: TypeMove, value: SelectType) => {
+    if (category === TypeMove.Fast) {
+      setFilters(Filter.create({ ...filters, fMoveType: value }));
+    } else {
+      setFilters(Filter.create({ ...filters, cMoveType: value }));
+    }
+  };
+
+  const setMoveNameByType = (category: TypeMove, value: string) => {
+    if (category === TypeMove.Fast) {
+      setFilters(Filter.create({ ...filters, fMoveName: value }));
+    } else {
+      setFilters(Filter.create({ ...filters, cMoveName: value }));
+    }
+  };
+
+  const moveList = (data: ICombat[], type: SelectType, name: string, moveLoad: boolean, category: TypeMove) => {
+    return (
+      <div className="xl:tw-flex-1 table-movesets-col !tw-p-0">
+        <table className="table-info table-movesets">
+          <thead />
+          <tbody>
+            <tr className="tw-text-center">
+              <td className="table-sub-header" colSpan={3}>
+                <div className="row !tw-m-0">
+                  <div className="!tw-w-1/3 tw-flex-none tw-flex tw-justify-center tw-items-center !tw-p-0">{`${getKeyWithData(
+                    TypeMove,
+                    category
+                  )} Moves List`}</div>
+                  <div className="!tw-w-1/3 tw-flex-none tw-flex tw-justify-center tw-items-center !tw-p-0">
+                    <SelectMui
+                      formClassName="tw-mt-2"
+                      formSx={{ m: 1, minWidth: 150 }}
+                      onChangeSelect={(value) => setMoveByType(category, toNumber(value))}
+                      value={type}
+                      inputLabel="Type"
+                      menuItems={[
+                        {
+                          value: SelectType.All,
+                          label: getKeyWithData(SelectType, SelectType.All),
+                          defaultChecked: true,
+                        },
+                        ...getTypes().map((value) => ({
+                          value: getDataWithKey<PokemonTypeBadge>(
+                            PokemonTypeBadge,
+                            value,
+                            EqualMode.IgnoreCaseSensitive
+                          ),
+                          label: capitalize(value),
+                        })),
+                      ]}
+                    />
+                  </div>
+                  <div className="!tw-w-1/3 tw-flex-none tw-flex tw-justify-center tw-items-center !tw-p-0">
+                    <InputMui
+                      placeholder="Enter Name or ID"
+                      defaultValue={name}
+                      onChange={(value) => setMoveNameByType(category, value)}
+                    />
+                  </div>
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <td className="data-table">
+                <CustomDataTable
+                  customColumns={columns}
+                  data={data}
+                  defaultSortFieldId={ColumnType.Name}
+                  fixedHeader
+                  fixedHeaderScrollHeight="70vh"
+                  progressPending={!moveLoad}
+                  progressComponent={<CircularProgressTable />}
+                />
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    );
+  };
+
+  return (
+    <div className="tw-container tw-my-4">
+      <div className="table-head">Moveset list in Pokémon GO</div>
+      <div className="row tw-w-full !tw-m-0">
+        {moveList(resultFMove, fMoveType, fMoveName, fMoveIsLoad, TypeMove.Fast)}
+        {moveList(resultCMove, cMoveType, cMoveName, cMoveIsLoad, TypeMove.Charge)}
+      </div>
+    </div>
+  );
+};
+
+export default Search;
