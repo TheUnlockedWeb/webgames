70d0ead691c10163aa4c491e97baba9c6901b5b3
EthanUnlocked <ethan.ioannou30@student.ccgs.nsw.edu.au>
2026-04-10 21:33:45 +1000

    Create arche-type.enum.ts

---
+++ pokegobreeze/src/pages/PVP/enums/arche-type.enum.ts
@@ -0,0 +1,14 @@
+export enum ArcheType {
+  SelfDebuff = 'Self-Debuff',
+  Debuff = 'Debuff',
+  General = 'General',
+  Nuke = 'Nuke',
+  SpamBait = 'Spam/Bait',
+  HighEnergy = 'High Energy',
+  LowQuality = 'Low Quality',
+  Boost = 'Boost',
+  FastCharge = 'Fast Charge',
+  HeavyDamage = 'Heavy Damage',
+  Multipurpose = 'Multipurpose',
+  DebuffSpamBait = 'Debuff Spam/Bait',
+}
