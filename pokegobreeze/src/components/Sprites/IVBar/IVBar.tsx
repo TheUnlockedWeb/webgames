899cdfd8eee18792c88eef88798ecbcf4c4ee695
EthanUnlocked <ethan.ioannou30@student.ccgs.nsw.edu.au>
2026-04-10 15:33:20 +1000

    Create IVBar.tsx

---
+++ pokegobreeze/src/components/Sprites/IVBar/IVBar.tsx
@@ -0,0 +1,58 @@
+import React, { useEffect, useState } from 'react';
+import './IVBar.scss';
+import { IIVBarComponent } from '../../models/component.model';
+import { combineClasses } from '../../../utils/extension';
+
+const IVBar = (props: IIVBarComponent) => {
+  const [ivWidthFirst, setIvWidthFirst] = useState(0);
+  const [ivWidthSec, setIvWidthSec] = useState(0);
+  const [ivWidthThird, setIvWidthThird] = useState(0);
+
+  useEffect(() => {
+    if (props.iv < 0) {
+      setIvWidthFirst(0);
+      setIvWidthSec(0);
+      setIvWidthThird(0);
+    }
+  }, [props.iv]);
+
+  useEffect(() => {
+    setIvWidthFirst(props.iv < 5 ? 20 * props.iv : 100);
+    setIvWidthSec(props.iv >= 5 ? (props.iv < 10 ? 20 * (props.iv - 5) : 100) : 0);
+    setIvWidthThird(props.iv >= 10 ? (props.iv < 15 ? 20 * (props.iv - 10) : 100) : 0);
+  }, [props.iv]);
+
+  return (
+    <div className="iv-container tw-mt-2" style={props.style}>
+      <div className="tw-flex tw-justify-between !tw-w-[99%]">
+        <b>{props.title}</b>
+        <b>{props.iv}</b>
+      </div>
+      <div className="iv-bg-group">
+        <div className="iv iv-first-child tw-relative">
+          <div
+            className={combineClasses(`tw-absolute iv-bar`, props.iv <= 5 ? 'border-right-iv' : '')}
+            style={{ width: `${ivWidthFirst}%` }}
+          />
+          <div className="iv-bg-bar !tw-w-full" />
+        </div>
+        <div className="iv tw-relative">
+          <div
+            className={combineClasses(`tw-absolute iv-bar`, props.iv > 5 && props.iv <= 10 ? 'border-right-iv' : '')}
+            style={{ width: `${ivWidthSec}%` }}
+          />
+          <div className="iv-bg-bar !tw-w-full" />
+        </div>
+        <div className="iv iv-last-child tw-relative">
+          <div
+            className={combineClasses(`tw-absolute iv-bar`, props.iv > 10 ? 'border-right-iv' : '')}
+            style={{ width: `${ivWidthThird}%` }}
+          />
+          <div className="iv-bg-bar !tw-w-full" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default IVBar;
