59bbae3dc1bf18d716eaba87b703b490c8482d32
EthanUnlocked <ethan.ioannou30@student.ccgs.nsw.edu.au>
2026-04-10 15:29:24 +1000

    Create CandyXL.tsx

---
+++ pokegobreeze/src/components/Sprites/Candy/CandyXL.tsx
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import { computeCandyBgColor, computeCandyColor } from '../../../utils/compute';
+
+import bgCandyXL from '../../../assets/bg_CandyXL.png';
+
+import { ICandyComponent } from '../../models/component.model';
+import { combineClasses, toNumber } from '../../../utils/extension';
+
+const DEFAULT_SIZE = 30;
+
+interface Element {
+  $candyColor?: string;
+  $candyBgColor?: string;
+  $size?: number;
+}
+
+const Background = styled.div<Element>`
+  position: absolute;
+  background: ${(props) => props.$candyBgColor};
+  clip-path: polygon(67% 17%, 75% 21%, 74% 66%, 19% 36%);
+  width: ${(props) => toNumber(props.$size, DEFAULT_SIZE)}px;
+  height: ${(props) => toNumber(props.$size, DEFAULT_SIZE)}px;
+`;
+
+const Fill = styled.div<Element>`
+  background: ${(props) => props.$candyColor};
+  width: ${(props) => toNumber(props.$size, DEFAULT_SIZE)}px;
+  height: ${(props) => toNumber(props.$size, DEFAULT_SIZE)}px;
+  mask: url(${bgCandyXL}) center/contain;
+  -webkit-mask: url(${bgCandyXL}) center/contain;
+  -moz-mask: url(${bgCandyXL}) center/contain;
+  -o-mask: url(${bgCandyXL}) center/contain;
+  -ms-mask: url(${bgCandyXL}) center/contain;
+`;
+
+const CandyXL = (props: ICandyComponent) => {
+  const [color, setColor] = useState<string>();
+  const [bgColor, setBgColor] = useState<string>();
+
+  useEffect(() => {
+    const candyColor = computeCandyColor(props.id);
+    const candyBgColor = computeCandyBgColor(props.id);
+    setColor(candyColor);
+    setBgColor(candyBgColor);
+  }, [props.id]);
+
+  return (
+    <div className={combineClasses('tw-relative tw-inline-block', props.className)} style={props.style}>
+      <Background $candyBgColor={bgColor} $size={props.size} />
+      <Fill $candyColor={color} $size={props.size} />
+    </div>
+  );
+};
+
+export default CandyXL;
