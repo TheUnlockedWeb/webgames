6dc26e625af62ed8b8a618f8ab52210e8825b326
EthanUnlocked <ethan.ioannou30@student.ccgs.nsw.edu.au>
2026-04-10 22:17:53 +1000

    Create constants.ts

---
+++ pokegobreeze/src/services/constants.ts
@@ -0,0 +1,44 @@
+export class APIUrl {
+  public static POKE_API_URL = 'https://pokeapi.co/api/v2/';
+  public static POKE_ASSETS = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
+  public static POGO_API_URL = 'https://pogoapi.net/api/v1/';
+
+  public static POGO_PROD_ASSET_URL = 'https://storage.googleapis.com/prod-public-images/';
+  public static POGO_PRODHOLOHOLO_ASSET_URL = 'https://prodholoholo-public-images.nianticlabs.com/';
+
+  public static POGO_ASSET_API_URL = 'https://raw.githubusercontent.com/PokeMiners/pogo_assets/master/Images/';
+  public static POGO_SOUND_API_URL = 'https://raw.githubusercontent.com/PokeMiners/pogo_assets/master/Sounds/';
+  public static POKE_SPRITES_API_URL = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
+
+  public static POKE_ICON_SPRITES_TYPE_API_URL = 'https://static.wikia.nocookie.net/pokemongo/images/';
+  public static POKE_SPRITES_FULL_API_URL =
+    'https://raw.githubusercontent.com/HybridShivam/Pokemon/master/assets/images/';
+
+  public static POKE_GIF_SPRITES_API_URL = 'https://raw.githubusercontent.com/argorar/Pokemon-Assets/master/Pokemon/';
+  public static POKE_TRAINER_SPRITES_API_URL =
+    'https://raw.githubusercontent.com/argorar/Pokemon-Assets/master/Trainers/';
+  public static POKE_SOUND_CRY_API_URL = 'https://raw.githubusercontent.com/Touched/pokedex-data/master/data/';
+  public static POKE_TYPES_API_URL =
+    'https://raw.githubusercontent.com/HybridShivam/Pokemon/master/assets/Others/type-icons/png-original/';
+
+  public static POKE_PV_API_URL = 'https://raw.githubusercontent.com/pvpoke/pvpoke/master/src/data/';
+  public static POKE_ASSETS_URL = `https://raw.githubusercontent.com/WHIZENx/pokemon-assets/master/`;
+
+  // Fetch Data
+  public static FETCH_POKEGO_IMAGES_ICON_SHA =
+    'https://api.github.com/repos/PokeMiners/pogo_assets/commits?path=Images/App%20Icons&page=1&per_page=1';
+  public static FETCH_POKEGO_IMAGES_POKEMON_SHA =
+    'https://api.github.com/repos/PokeMiners/pogo_assets/commits?path=Images/Pokemon%20-%20256x256&page=1&per_page=1';
+  public static FETCH_POKEGO_IMAGES_SOUND_SHA =
+    'https://api.github.com/repos/PokeMiners/pogo_assets/commits?path=Sounds/Pokemon%20Cries&page=1&per_page=1';
+
+  public static FETCH_PVP_DATA = 'https://api.github.com/repos/pvpoke/pvpoke/commits?path=src/data&page=1&per_page=1';
+
+  public static TIMESTAMP = 'https://raw.githubusercontent.com/PokeMiners/game_masters/master/latest/timestamp.txt';
+  public static GAMEMASTER = 'https://raw.githubusercontent.com/PokeMiners/game_masters/master/latest/latest.json';
+  public static CANDY_DATA =
+    'https://raw.githubusercontent.com/PokeMiners/pogo_assets/master/Candy%20Color%20Data/PokemonCandyColorData.json';
+
+  public static FETCH_PVP_MOVES =
+    'https://raw.githubusercontent.com/pvpoke/pvpoke/master/src/data/gamemaster/moves.json';
+}
