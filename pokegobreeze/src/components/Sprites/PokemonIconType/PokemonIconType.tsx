08c72dc78893d2f21e584e3af250452334282d6c
EthanUnlocked <ethan.ioannou30@student.ccgs.nsw.edu.au>
2026-04-10 15:38:19 +1000

    Create PokemonIconType.tsx

---
+++ pokegobreeze/src/components/Sprites/PokemonIconType/PokemonIconType.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { IPokemonIconTypeComponent } from '../../models/component.model';
+import { PokemonType } from '../../../enums/type.enum';
+import APIService from '../../../services/api.service';
+import { combineClasses } from '../../../utils/extension';
+
+const PokemonIconType = (props: IPokemonIconTypeComponent) => {
+  const getIconType = (src: string, className: string, alt: string) => (
+    <img
+      className={combineClasses(className, props.className)}
+      alt={alt}
+      style={props.style}
+      height={props.size}
+      src={src}
+    />
+  );
+
+  return (
+    <>
+      {props.pokemonType === PokemonType.Shadow && getIconType(APIService.getPokeShadow(), 'shadow-icon', 'img-shadow')}
+      {props.pokemonType === PokemonType.Purified &&
+        getIconType(APIService.getPokePurified(), 'purified-icon', 'img-purified')}
+      {props.children}
+    </>
+  );
+};
+
+export default PokemonIconType;
