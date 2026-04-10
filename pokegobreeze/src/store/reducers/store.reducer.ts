69af898a3bee96a21047bb5d9eea9915ca1bd9a3
EthanUnlocked <ethan.ioannou30@student.ccgs.nsw.edu.au>
2026-04-10 22:21:57 +1000

    Create store.reducer.ts

---
+++ pokegobreeze/src/store/reducers/store.reducer.ts
@@ -0,0 +1,143 @@
+import { convertPVPRankings, convertPVPTrain } from '../../core/pvp';
+import { replaceTempMovePvpName } from '../../utils/utils';
+import { Store } from '../models/store.model';
+import { StoreActions } from '../actions';
+import { StoreActionsUnion } from '../actions/store.action';
+import { isEqual } from '../../utils/extension';
+
+const initialize = new Store();
+
+const StoreReducer = (state = initialize, action: StoreActionsUnion) => {
+  switch (action.type) {
+    case StoreActions.StoreActionTypes.getStore:
+      return state;
+    case StoreActions.StoreActionTypes.setCPM:
+      return {
+        ...state,
+        data: {
+          ...state.data,
+          cpm: action.payload,
+        },
+      };
+    case StoreActions.StoreActionTypes.setLogoPokeGO:
+      return {
+        ...state,
+        icon: action.payload.replace('Images/App Icons/', '').replace('.png', ''),
+      };
+    case StoreActions.StoreActionTypes.setOptions:
+      return {
+        ...state,
+        data: {
+          ...state.data,
+          options: action.payload,
+        },
+      };
+    case StoreActions.StoreActionTypes.setPokemon:
+      return {
+        ...state,
+        data: {
+          ...state.data,
+          pokemons: action.payload,
+        },
+      };
+    case StoreActions.StoreActionTypes.setSticker:
+      return {
+        ...state,
+        data: {
+          ...state.data,
+          stickers: action.payload,
+        },
+      };
+    case StoreActions.StoreActionTypes.setCombat:
+      return {
+        ...state,
+        data: {
+          ...state.data,
+          combats: action.payload,
+        },
+      };
+    case StoreActions.StoreActionTypes.setEvolutionChain:
+      return {
+        ...state,
+        data: {
+          ...state.data,
+          evolutionChains: action.payload,
+        },
+      };
+    case StoreActions.StoreActionTypes.setInformation:
+      return {
+        ...state,
+        data: {
+          ...state.data,
+          information: {
+            data: action.payload,
+            isLoaded: true,
+          },
+        },
+      };
+    case StoreActions.StoreActionTypes.setAssets:
+      return {
+        ...state,
+        data: {
+          ...state.data,
+          assets: action.payload,
+        },
+      };
+    case StoreActions.StoreActionTypes.setLeagues:
+      return {
+        ...state,
+        data: {
+          ...state.data,
+          leagues: action.payload,
+        },
+      };
+    case StoreActions.StoreActionTypes.setTrainers:
+      return {
+        ...state,
+        data: {
+          ...state.data,
+          trainers: action.payload,
+        },
+      };
+    case StoreActions.StoreActionTypes.setPVP:
+      return {
+        ...state,
+        data: {
+          ...state.data,
+          pvp: {
+            rankings: convertPVPRankings(action.payload.rankings, state.data.leagues.data),
+            trains: convertPVPTrain(action.payload.trains, state.data.leagues.data),
+          },
+        },
+      };
+    case StoreActions.StoreActionTypes.setPVPMoves: {
+      const result = state.data.combats.map((move) => {
+        const movePVP = action.payload.find((data) =>
+          isEqual(
+            data.moveId,
+            isEqual(move.name, 'HIDDEN_POWER') ? 'HIDDEN_POWER_BUG' : replaceTempMovePvpName(move.name)
+          )
+        );
+        move.archetype = movePVP?.archetype;
+        move.abbreviation = movePVP?.abbreviation;
+        return move;
+      });
+      return {
+        ...state,
+        data: {
+          ...state.data,
+          combats: result,
+        },
+      };
+    }
+    case StoreActions.StoreActionTypes.resetStore:
+      return {
+        ...initialize,
+        timestamp: new Date().getTime(),
+      };
+    default:
+      return state;
+  }
+};
+
+export default StoreReducer;
