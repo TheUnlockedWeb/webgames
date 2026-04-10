f58e0461c6a89413db009539501f6002ec7e2a35
EthanUnlocked <ethan.ioannou30@student.ccgs.nsw.edu.au>
2026-04-10 15:30:58 +1000

    Create CircularProgress.tsx

---
+++ pokegobreeze/src/components/Sprites/CircularProgress/CircularProgress.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import CircularProgress from '@mui/material/CircularProgress';
+
+const CircularProgressTable = () => {
+  return (
+    <div className="tw-bg-custom-default tw-w-full tw-h-full tw-flex tw-justify-center tw-items-center">
+      <div className="tw-m-2">
+        <CircularProgress />
+      </div>
+    </div>
+  );
+};
+
+export default CircularProgressTable;
