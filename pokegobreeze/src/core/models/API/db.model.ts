1d9835794293fcda9dbfa65d76cfc702de9c9538
EthanUnlocked <ethan.ioannou30@student.ccgs.nsw.edu.au>
2026-04-10 19:37:58 +1000

    Create db.model.ts

---
+++ pokegobreeze/src/core/models/API/db.model.ts
@@ -0,0 +1,27 @@
+interface IDatabase<T> {
+  command: string;
+  fields: IField[];
+  rowAsArray?: boolean;
+  rowCount: number;
+  rows: T[];
+  viaNeonFetch?: boolean;
+}
+
+interface IField {
+  columnID: number;
+  dataTypeID: number;
+  dataTypeModifier: number;
+  dataTypeSize: number;
+  format: string;
+  name: string;
+  tableID: number;
+}
+
+export class Database<T> implements IDatabase<T> {
+  command = '';
+  fields: IField[] = [];
+  rowAsArray?: boolean;
+  rowCount = 0;
+  rows: T[] = [];
+  viaNeonFetch?: boolean;
+}
