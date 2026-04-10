67c1318b5ffb9b953dff5323baad66f67198bc15
EthanUnlocked <ethan.ioannou30@student.ccgs.nsw.edu.au>
2026-04-10 11:45:53 +1000

    Create .eslintrc.js

---
+++ pokegobreeze/.eslintrc.js
@@ -0,0 +1,91 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+    warnOnUnsupportedTypeScriptVersion: false,
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  ignorePatterns: ['**/*.css', '**/*.scss'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react/recommended', 'prettier'],
+  plugins: ['@typescript-eslint', 'react', 'prettier', 'css', 'stylelint'],
+  rules: {
+    '@typescript-eslint/no-explicit-any': 0,
+    curly: 1,
+    'no-console': 'warn',
+    'prettier/prettier': [
+      'warn',
+      {
+        singleQuote: true,
+        semi: true,
+        tabWidth: 2,
+        printWidth: 120,
+        trailingComma: 'es5',
+      },
+    ],
+  },
+  overrides: [
+    {
+      files: ['src/**/*.ts', 'src/**/*.tsx'],
+      parserOptions: {
+        project: './tsconfig.app.json',
+      },
+    },
+    {
+      files: ['vite.config.ts'],
+      parserOptions: {
+        project: './tsconfig.node.json',
+      },
+      rules: {
+        '@typescript-eslint/no-var-requires': 'off',
+      },
+    },
+    {
+      files: ['*.js'],
+      env: {
+        node: true,
+      },
+      rules: {
+        '@typescript-eslint/no-var-requires': 'off',
+      },
+    },
+    {
+      files: ['*.css'],
+      plugins: ['css'],
+      extends: ['plugin:css/recommended'],
+      rules: {
+        'prettier/prettier': [
+          'warn',
+          {
+            singleQuote: true,
+            semi: true,
+            tabWidth: 2,
+            printWidth: 120,
+          },
+        ],
+      },
+    },
+    {
+      files: ['*.scss'],
+      plugins: ['stylelint'],
+      rules: {
+        'prettier/prettier': [
+          'warn',
+          {
+            singleQuote: true,
+            semi: true,
+            tabWidth: 2,
+            printWidth: 120,
+          },
+        ],
+      },
+    },
+  ],
+};
