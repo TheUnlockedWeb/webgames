1b38f44877d3633b3c30faebf3434697b03a55dc
EthanUnlocked <ethan.ioannou30@student.ccgs.nsw.edu.au>
2026-04-10 12:54:07 +1000

    Create CardPokemonInfo.tsx

---
+++ pokegobreeze/src/components/Card/CardPokemonInfo.tsx
@@ -0,0 +1,141 @@
+import React, { useState, useRef } from 'react';
+import { computeBgType } from '../../utils/compute';
+import { generateParamForm, getValidPokemonImgPath, splitAndCapitalize } from '../../utils/utils';
+import TypeInfo from '../Sprites/Type/Type';
+import './CardPokemonInfo.scss';
+
+import APIService from '../../services/api.service';
+import { ICardPokemonInfoComponent } from '../models/component.model';
+import { combineClasses } from '../../utils/extension';
+import { PokemonType } from '../../enums/type.enum';
+import { LinkToTop } from '../Link/LinkToTop';
+import { useDevice } from '../../composables/useDevice';
+
+const CardPokemonInfo = (props: ICardPokemonInfoComponent) => {
+  const [isShiny, setIsShiny] = useState(false);
+  const { isMobile } = useDevice();
+
+  const imageRef: React.LegacyRef<HTMLImageElement> = useRef(null);
+  const shinyRef: React.LegacyRef<HTMLImageElement> = useRef(null);
+
+  const onTouchEnd = () => {
+    if (!isMobile || props.isDefaultImg) {
+      return;
+    }
+
+    if (isShiny) {
+      shinyRef.current?.classList.remove('active');
+    } else {
+      shinyRef.current?.classList.add('active');
+    }
+    setIsShiny(!isShiny);
+  };
+
+  const onHoverShiny = () => {
+    if (isMobile || props.isDefaultImg) {
+      return;
+    }
+
+    shinyRef.current?.classList.add('active');
+    setIsShiny(true);
+  };
+
+  const onLeaveShiny = () => {
+    if (isMobile || props.isDefaultImg) {
+      return;
+    }
+
+    shinyRef.current?.classList.remove('active');
+    setIsShiny(false);
+  };
+
+  return (
+    <li
+      className="tw-relative border-types tw-h-full"
+      style={{ backgroundImage: computeBgType(props.types, PokemonType.Normal, props.styleList, 0.3) }}
+    >
+      {!props.releasedGO && (
+        <div className="no-released-pokemon">
+          <img
+            width={24}
+            height={24}
+            title="Coming Soon"
+            alt="Pokémon GO Icon"
+            src={APIService.getPokemonGoIcon(props.icon)}
+          />
+        </div>
+      )}
+      {props.image.shiny && (
+        <img
+          onTouchEnd={onTouchEnd}
+          onMouseOver={onHoverShiny}
+          onMouseLeave={onLeaveShiny}
+          ref={shinyRef}
+          className={combineClasses('shiny-pokemon', props.isDefaultImg ? 'active' : '')}
+          height={32}
+          src={APIService.getShinyIcon()}
+          title="Shiny Pokemon"
+          alt="Icon Shiny"
+        />
+      )}
+      <LinkToTop
+        className="tw-block tw-h-full pokemon-link"
+        to={`/pokemon/${props.id}${generateParamForm(props.form)}`}
+      >
+        <div className="tw-h-full tw-flex tw-flex-col tw-justify-between tw-gap-2">
+          <div>
+            <div className="tw-flex tw-justify-center tw-p-2">
+              <span className="tw-w-24">
+                <img
+                  ref={imageRef}
+                  className="pokemon-sprite-large"
+                  alt={`#${props.id} ${splitAndCapitalize(props.name.replaceAll('_', '-'), '-', ' ')}`}
+                  title={`#${props.id} ${splitAndCapitalize(props.name.replaceAll('_', '-'), '-', ' ')}`}
+                  src={props.image.shiny && (isShiny || props.isDefaultImg) ? props.image.shiny : props.image.default}
+                  onError={(e) => {
+                    const form =
+                      props.image.shiny && (isShiny || props.isDefaultImg) ? props.image.shiny : props.image.default;
+                    const formList = form.split('/');
+                    e.currentTarget.onerror = null;
+                    e.currentTarget.src = getValidPokemonImgPath(
+                      e.currentTarget.src,
+                      props.id,
+                      formList[formList.length - 1].replace('.png', '')
+                    );
+                  }}
+                />
+              </span>
+            </div>
+            <TypeInfo arr={props.types} isHideText height={24} />
+            <b>
+              <span className="tw-text-sm tw-text-center tw-text-default">{`#${props.id} ${splitAndCapitalize(
+                props.name.replaceAll('_', '-'),
+                '-',
+                ' '
+              )}`}</span>
+            </b>
+          </div>
+          <div>
+            <div className="tw-flex tw-items-center tw-justify-center tw-w-full">
+              <b>
+                <span className="caption !tw-text-red-600">{`ATK ${props.pokemonStat.atk}`}</span>
+              </b>
+            </div>
+            <div className="tw-flex tw-items-center tw-justify-center tw-w-full">
+              <b>
+                <span className="caption !tw-text-green-600">{`DEF ${props.pokemonStat.def}`}</span>
+              </b>
+            </div>
+            <div className="tw-flex tw-items-center tw-justify-center tw-w-full">
+              <b>
+                <span className="caption !tw-text-cyan-600">{`STA ${props.pokemonStat.sta}`}</span>
+              </b>
+            </div>
+          </div>
+        </div>
+      </LinkToTop>
+    </li>
+  );
+};
+
+export default CardPokemonInfo;
