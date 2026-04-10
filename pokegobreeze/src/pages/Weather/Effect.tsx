a1f88f01e91a352d66d1191887e25980f80b15aa
EthanUnlocked <ethan.ioannou30@student.ccgs.nsw.edu.au>
2026-04-10 22:15:15 +1000

    Create Effect.tsx

---
+++ pokegobreeze/src/pages/Weather/Effect.tsx
@@ -0,0 +1,68 @@
+import React, { useCallback, useEffect, useState } from 'react';
+import { getPropertyName, isIncludeList, isNotEmpty, safeObjectEntries } from '../../utils/extension';
+import { getWeatherBoost, getTypeEffective } from '../../utils/helpers/options-context.helpers';
+import SelectTypeComponent from '../../components/Commons/Selects/SelectType';
+import { camelCase } from '../../utils/utils';
+import Effective from '../../components/Effective/Effective';
+import Weather from '../../components/Sprites/Weather';
+
+const Effect = () => {
+  const weathersBoost = getWeatherBoost();
+  const typesEffective = getTypeEffective();
+
+  const [currentTypePri, setCurrentTypePri] = useState(camelCase(getPropertyName(typesEffective, (o) => o.bug)));
+  const [currentTypeSec, setCurrentTypeSec] = useState('');
+
+  const [weatherEffective, setWeatherEffective] = useState<string[]>([]);
+
+  const getWeatherEffective = useCallback(() => {
+    const data: string[] = [];
+    safeObjectEntries(weathersBoost).forEach(([key, value]) => {
+      if (isIncludeList(value, currentTypePri) && !isIncludeList(data, key)) {
+        data.push(key);
+      }
+      if (isNotEmpty(currentTypeSec) && isIncludeList(value, currentTypeSec) && !isIncludeList(data, key)) {
+        data.push(key);
+      }
+    });
+    setWeatherEffective(data);
+  }, [currentTypePri, currentTypeSec, weathersBoost]);
+
+  useEffect(() => {
+    getWeatherEffective();
+  }, [currentTypePri, currentTypeSec, getWeatherEffective, typesEffective]);
+
+  return (
+    <div className="tw-mt-2">
+      <h5 className="tw-text-center">
+        <b>As Pokémon Types</b>
+      </h5>
+      <div className="row">
+        <div className="col tw-flex tw-justify-center">
+          <SelectTypeComponent
+            title="Type 1"
+            data={typesEffective}
+            currentType={currentTypePri}
+            setCurrentType={setCurrentTypePri}
+            filterType={[currentTypePri, currentTypeSec]}
+          />
+        </div>
+        <div className="col tw-flex tw-justify-center">
+          <SelectTypeComponent
+            title="Type 2"
+            data={typesEffective}
+            currentType={currentTypeSec}
+            setCurrentType={setCurrentTypeSec}
+            filterType={[currentTypeSec, currentTypePri]}
+            isShowRemove
+          />
+        </div>
+      </div>
+      <Effective title="Weather Boosts">
+        <Weather arr={weatherEffective} className="tw-ml-3" />
+      </Effective>
+    </div>
+  );
+};
+
+export default Effect;
