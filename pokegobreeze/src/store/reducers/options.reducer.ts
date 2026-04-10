e261fb509e4008f4589cbf99cc8c5fba9b507ee9
EthanUnlocked <ethan.ioannou30@student.ccgs.nsw.edu.au>
2026-04-10 22:20:18 +1000

    Create options.reducer.ts

---
+++ pokegobreeze/src/store/reducers/options.reducer.ts
@@ -0,0 +1,22 @@
+import { OptionsSheetModel } from '../../core/models/options-sheet.model';
+import { OptionsActions } from '../actions';
+import { OptionsActionsUnion } from '../actions/options.action';
+
+const OptionsReducer = (state: OptionsSheetModel | null = null, action: OptionsActionsUnion) => {
+  switch (action.type) {
+    case OptionsActions.OptionsActionTypes.setDpsSheetOptions:
+      return {
+        ...state,
+        dpsSheet: action.payload,
+      };
+    case OptionsActions.OptionsActionTypes.setCounterOptions:
+      return {
+        ...state,
+        counter: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+export default OptionsReducer;
