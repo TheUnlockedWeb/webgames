97d34693ed7d5d52bcb5e0a4283724543047061c
EthanUnlocked <ethan.ioannou30@student.ccgs.nsw.edu.au>
2026-04-10 17:19:33 +1000

    Create league.enum.ts

---
+++ pokegobreeze/src/core/enums/league.enum.ts
@@ -0,0 +1,28 @@
+export enum RewardType {
+  Pokemon = 'pokemon',
+  ItemLoot = 'itemLoot',
+  Stardust = 'stardust',
+  RareCandy = 'ITEM_RARE_CANDY',
+  MoveReRoll = 'ITEM_MOVE_REROLL_SPECIAL_ATTACK',
+}
+
+export enum LeagueRewardType {
+  Free,
+  Premium,
+}
+
+export enum LeagueBattleType {
+  None = 'none',
+  All = 'all',
+  Remix = 'remix',
+  Little = 'little',
+  Great = 'great',
+  Ultra = 'ultra',
+  Master = 'master',
+  Mega = 'mega',
+}
+
+export enum LeagueType {
+  Standard,
+  Premier,
+}
