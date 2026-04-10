7be93747c0c5d2139d71f23b5e173d6c2881576b
EthanUnlocked <ethan.ioannou30@student.ccgs.nsw.edu.au>
2026-04-10 20:17:33 +1000

    Create news.model.ts

---
+++ pokegobreeze/src/pages/News/models/news.model.ts
@@ -0,0 +1,36 @@
+import { IInformation, ITicketReward } from '../../../core/models/information';
+import { DateEvent } from '../enums/item-type.enum';
+
+export interface IRewardNews extends Partial<ITicketReward> {
+  imageSrc: string | undefined;
+  title: string;
+  count: number;
+}
+
+export class RewardNews implements IRewardNews {
+  imageSrc: string | undefined;
+  title = '';
+  count = 0;
+
+  static create(value: IRewardNews) {
+    const obj = new RewardNews();
+    Object.assign(obj, value);
+    return obj;
+  }
+}
+
+export interface INewsModel extends Partial<IInformation> {
+  eventType: DateEvent;
+  rewardNews: IRewardNews[];
+}
+
+export class NewsModel implements INewsModel {
+  eventType = DateEvent.None;
+  rewardNews: IRewardNews[] = [];
+
+  static create(value: INewsModel) {
+    const obj = new NewsModel();
+    Object.assign(obj, value);
+    return obj;
+  }
+}
