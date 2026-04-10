f09b91603ba11bc94bf07f043fca80c4ca2d37c2
EthanUnlocked <ethan.ioannou30@student.ccgs.nsw.edu.au>
2026-04-10 22:13:21 +1000

    Create Attacker.tsx

---
+++ pokegobreeze/src/pages/TypeEffect/Attacker.tsx
@@ -0,0 +1,49 @@
+import React, { useEffect, useState, useCallback } from 'react';
+import TypeEffectiveComponent from '../../components/Effective/TypeEffective';
+import { camelCase, getMultiplyTypeEffect } from '../../utils/utils';
+import { ITypeEffectiveChart, TypeEffectiveChart } from '../../core/models/type-effective.model';
+import { getPropertyName } from '../../utils/extension';
+import {
+  getTypeEffective as getTypeEffectiveContext,
+  getSafeTypesEffective,
+} from '../../utils/helpers/options-context.helpers';
+import SelectTypeComponent from '../../components/Commons/Selects/SelectType';
+
+const Attacker = () => {
+  const typesEffective = getTypeEffectiveContext();
+  const [currentType, setCurrentType] = useState(camelCase(getPropertyName(typesEffective, (o) => o.bug)));
+
+  const [typeEffective, setTypeEffective] = useState<ITypeEffectiveChart>();
+
+  const getTypeEffective = useCallback(() => {
+    const data = new TypeEffectiveChart();
+    getSafeTypesEffective(currentType).forEach(([key, value]) => getMultiplyTypeEffect(data, value, key));
+    setTypeEffective(data);
+  }, [currentType, typesEffective]);
+
+  useEffect(() => {
+    getTypeEffective();
+  }, [currentType, getTypeEffective, typesEffective]);
+
+  return (
+    <div className="tw-mt-2">
+      <h5 className="tw-text-center">
+        <b>As Attacker</b>
+      </h5>
+      <div className="row">
+        <div className="col tw-flex tw-justify-center">
+          <SelectTypeComponent
+            title="Select Type"
+            data={typesEffective}
+            currentType={currentType}
+            setCurrentType={setCurrentType}
+            filterType={[currentType]}
+          />
+        </div>
+      </div>
+      <TypeEffectiveComponent typeEffective={typeEffective} />
+    </div>
+  );
+};
+
+export default Attacker;
